4144f332a30145f2059def7625215962585c3682
DataNick <DataNick@users.noreply.github.com>
2016-01-22 14:39:44 -0500

    Added new  variable

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var app = angular.module('flapperNews', []);
 app.controller('MainCtrl', [
   '$scope',
   function ($scope) {
-    $scope.test = 'Hello. I\'m listening'
-  }
-  ])
+    $scope.test = 'Hello. I\'m listening',
+    $scope.posts = ['post 1', 'post 2', 'post 3', 'post 4', 'post 5']
+  }]);
+
+// $scope is the bridge between controller and template
