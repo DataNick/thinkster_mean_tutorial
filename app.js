6fecb3fca6fe2de2fdfbf30fc360c57f7b9a0146
DataNick <DataNick@users.noreply.github.com>
2016-01-22 18:36:19 -0500

    Added new function to controller to add posts

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ app.controller('MainCtrl', [
   '$scope',
   function ($scope) {
     $scope.test = 'Hello. I\'m listening',
-    $scope.posts = [{ title: 'post 1', upvotes: 5 }, { title: 'post 2', upvotes: 2 }, { title: 'post 3', upvotes: 15 }, { title: 'post 4', upvotes: 9 }, { title: 'post 5', upvotes: 4 }]
+    $scope.posts = [{ title: 'post 1', upvotes: 5 }, { title: 'post 2', upvotes: 2 }, { title: 'post 3', upvotes: 15 }, { title: 'post 4', upvotes: 9 }, { title: 'post 5', upvotes: 4 }],
+    $scope.addPost = function () {
+      $scope.posts.push({ title: 'A new post', upvotes: 0 });
+    }
   }]);
 
 // $scope is the bridge between controller and template; to make something available in template, bind it to $scope
