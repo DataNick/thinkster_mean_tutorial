b3c589fed1487935e7b48436fcd59a260407872a
DataNick <DataNick@users.noreply.github.com>
2016-01-22 18:54:09 -0500

    Added condition to prevent posting a blank title

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.controller('MainCtrl', [
     $scope.test = 'Hello. I\'m listening';
     $scope.posts = [{ title: 'post 1', upvotes: 5 }, { title: 'post 2', upvotes: 2 }, { title: 'post 3', upvotes: 15 }, { title: 'post 4', upvotes: 9 }, { title: 'post 5', upvotes: 4 }];
     $scope.addPost = function () {
+      if(!$scope.title || $scope.title === '') { return; }
       $scope.posts.push({ title: $scope.title, upvotes: 0 });
       $scope.title = '';
     }
