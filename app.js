91e39681f8ef0231fd8c138e43b7867bc8b87bb7
DataNick <DataNick@users.noreply.github.com>
2016-01-23 14:13:22 -0500

    Bound .posts to posts.posts

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var app = angular.module('flapperNews', []);
 
 app.factory('posts', [function () {
   var o = {
-    posts: [];
+    posts: []
   };
   return o;
 }]);
@@ -11,7 +11,7 @@ app.controller('MainCtrl', [
   '$scope', 'posts',
   function ($scope, posts) {
     $scope.test = 'Hello. I\'m listening';
-    $scope.posts = [{ title: 'post 1', upvotes: 5 }, { title: 'post 2', upvotes: 2 }, { title: 'post 3', upvotes: 15 }, { title: 'post 4', upvotes: 9 }, { title: 'post 5', upvotes: 4 }];
+    $scope.posts = posts.posts;
     $scope.addPost = function () {
       if(!$scope.title || $scope.title === '') { return; }
       $scope.posts.push({ title: $scope.title, link: $scope.link, upvotes: 0 });
