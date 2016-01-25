d3af68625a6db36b1f66f288159b2f071da118f4
DataNick <DataNick@users.noreply.github.com>
2016-01-24 19:45:10 -0500

    Cleaned up syntax

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -31,7 +31,7 @@ router.post('/posts', function(req, res, next) {
 });
 
 router.param('post', function (req, res, next, id) {
-  var query = Post.findById(:id);
+  var query = Post.findById(id);
 
   query.exec(function (err, post) {
     if (err) {return next(err); }
@@ -45,3 +45,13 @@ router.param('post', function (req, res, next, id) {
 router.get('/posts/:post', function (req, res){
   res.json(req.post);
 });
+
+router.put('/posts/:post/upvote', function (req, res, next) {
+  req.post.upvote(function (err, post){
+    if (err) {return next(err); }
+
+    res.json(post);
+  });
+});
+
+
